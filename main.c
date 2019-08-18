13ff90e37451b9c14fa43cc670a6e1511ef8d614
Franz Flasch <franz.flasch@gmx.at>
2019-08-18 23:42:37 +0200

    Added separate debug prints

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 /* NES specific */
+#include <nes.h>
 #include <memory.h>
 #include <ppu.h>
 #include <cpu.h>
@@ -9,13 +10,16 @@
 
 #include <allegro5/allegro.h>
 
+#define debug_print(fmt, ...) \
+            do { if (DEBUG_MAIN) printf(fmt, __VA_ARGS__); } while (0)
+
 ALLEGRO_BITMAP *color_pallete = NULL;
 
 ppu_color_pallete_2C02_t color_pallete_2C02[] = {
- { 84, 84, 84}, {  0, 30,116}, {  8, 16,144}, { 48,  0,136}, { 68,  0,100}, { 92,  0, 48}, { 84,  4,  0}, { 60, 24,  0}, { 32, 42,  0}, {  8, 58,  0}, {  0, 64,  0}, {  0, 60,  0}, {  0, 50, 60}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},
- {152,150,152}, {  8, 76,196}, { 48, 50,236}, { 92, 30,228}, {136, 20,176}, {160, 20,100}, {152, 34, 32}, {120, 60,  0}, { 84, 90,  0}, { 40,114,  0}, {  8,124,  0}, {  0,118, 40}, {  0,102,120}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},
- {236,238,236}, { 76,154,236}, {120,124,236}, {176, 98,236}, {228, 84,236}, {236, 88,180}, {236,106,100}, {212,136, 32}, {160,170,  0}, {116,196,  0}, { 76,208, 32}, { 56,204,108}, { 56,180,204}, { 60, 60, 60}, {  0,  0,  0}, {  0,  0,  0},
- {236,238,236}, {168,204,236}, {188,188,236}, {212,178,236}, {236,174,236}, {236,174,212}, {236,180,176}, {228,196,144}, {204,210,120}, {180,222,120}, {168,226,144}, {152,226,180}, {160,214,228}, {160,162,160}, {  0,  0,  0}, {  0,  0,  0}
+    { 84, 84, 84}, {  0, 30,116}, {  8, 16,144}, { 48,  0,136}, { 68,  0,100}, { 92,  0, 48}, { 84,  4,  0}, { 60, 24,  0}, { 32, 42,  0}, {  8, 58,  0}, {  0, 64,  0}, {  0, 60,  0}, {  0, 50, 60}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},
+    {152,150,152}, {  8, 76,196}, { 48, 50,236}, { 92, 30,228}, {136, 20,176}, {160, 20,100}, {152, 34, 32}, {120, 60,  0}, { 84, 90,  0}, { 40,114,  0}, {  8,124,  0}, {  0,118, 40}, {  0,102,120}, {  0,  0,  0}, {  0,  0,  0}, {  0,  0,  0},
+    {236,238,236}, { 76,154,236}, {120,124,236}, {176, 98,236}, {228, 84,236}, {236, 88,180}, {236,106,100}, {212,136, 32}, {160,170,  0}, {116,196,  0}, { 76,208, 32}, { 56,204,108}, { 56,180,204}, { 60, 60, 60}, {  0,  0,  0}, {  0,  0,  0},
+    {236,238,236}, {168,204,236}, {188,188,236}, {212,178,236}, {236,174,236}, {236,174,212}, {236,180,176}, {228,196,144}, {204,210,120}, {180,222,120}, {168,226,144}, {152,226,180}, {160,214,228}, {160,162,160}, {  0,  0,  0}, {  0,  0,  0}
 };
 
 int init_allegro(void);
@@ -261,8 +265,8 @@ int main(int argc, char *argv[])
 
 
 
-
-    const float FPS = 60;
+    /* FIXME: TURN OFF debug prints if setting FPS to a high value otherwise won't work */
+    const float FPS = 5;
     const int SCREEN_W = 256*5;
     const int SCREEN_H = 240*5;
 
@@ -351,27 +355,27 @@ int main(int argc, char *argv[])
             /* Nametable 0 contents */
             for(i=0x2000;i<0x23FF;i++)
             {
-                printf("Nametable 0: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
+                debug_print("Nametable 0: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
             }
 
             for(i=0x2400;i<0x27FF;i++)
             {
-                printf("Nametable 1: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
+                debug_print("Nametable 1: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
             }
 
             for(i=0x2800;i<0x2BFF;i++)
             {
-                printf("Nametable 2: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
+                debug_print("Nametable 2: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
             }
 
             for(i=0x2C00;i<0x2FFF;i++)
             {
-                printf("Nametable 3: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
+                debug_print("Nametable 3: %x %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
             }
 
             for(i=0x3F00;i<0x3F1F;i++)
             {
-                printf("Color Pallete %x: %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
+                debug_print("Color Pallete %x: %x\n", i, *nes_ppu.memmap->ppu_mem_map.mem_virt[i]);
             }
 
             /* Draw color pallete */
